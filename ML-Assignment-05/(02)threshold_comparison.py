3e5b3623923246b587d408c871b8ab0d72802e58
Kahkasha Naaz <kahkashanaaz2005@gmail.com>
2026-09-23 17:37:59 +0530

    (02)threshold_comparison.py
    Added recall calculation for different thresholds.

--- ML-Assignment-05/(02)threshold_comparison.py
+++ ML-Assignment-05/(02)threshold_comparison.py
@@ -3,8 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_score, recall_score
-
-# 1. Create Student Dataset
 data = {
     "Study_Hours": [2, 3, 4, 5, 6, 7, 8, 1, 2, 5,
                     6, 7, 3, 8, 4, 9, 1, 6, 7, 3],
@@ -15,33 +13,19 @@ data = {
     "Pass": [0, 0, 0, 1, 1, 1, 1, 0, 0, 1,
              1, 1, 0, 1, 0, 1, 0, 1, 1, 0]
 }
-
 df = pd.DataFrame(data)
-
-# 2. Features and Target
 X = df[["Study_Hours", "Attendance"]]
 y = df["Pass"]
-
-# 3. Train-Test Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-
-# 4. Feature Scaling
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
-
-# 5. Train Logistic Regression
 model = LogisticRegression()
 model.fit(X_train, y_train)
-
-# 6. Get probability of Pass
 probability = model.predict_proba(X_test)[:, 1]
-
-# 7. Test Different Thresholds
 for threshold in [0.3, 0.5, 0.7]:
-
     y_pred = (probability >= threshold).astype(int)
 
     precision = precision_score(y_test, y_pred, zero_division=0)
